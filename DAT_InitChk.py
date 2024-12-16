b6c5f48015079bcafd4e44202651415da21f5ddd
sgaobnl <gao33.bnl@gmail.com>
2024-12-16 16:25:31 -0500

    Update DAT_InitChk.py

--- DAT_InitChk.py
+++ DAT_InitChk.py
@@ -457,14 +457,14 @@ def dat_initchk(fdir="/."):
                 pwr_meas_adc = cfgdata[4]
                 pwr_meas_cd = cfgdata[5]
                 bads1 = ana_fepwr2(pwr_meas_fe, vin=[1.7,1.9], cdda=[10,25], cddp=[25,35], cddo=[-0.1,5])
-                bads2 = ana_adcpwr(pwr_meas_adc, v2p5 = [2.10, 2.35], v1p2 = [1.05, 1.15], ca2p5=[115,145], cd1p2=[0,5], cio=[3,10], cd2p5 = [15,25])
+                bads2 = ana_adcpwr(pwr_meas_adc, v2p5 = [2.10, 2.35], v1p2 = [1.05, 1.15], ca2p5=[115,145], cd1p2=[0,5], cio=[3,10], cd2p5 = [10,25])
                 bads3 = ana_cdpwr(pwr_meas_cd, vddfe = [1.7, 1.9], v1p1 = [1.15, 1.25], vddio = [2.15, 2.35], cdda = [7, 11], cddfe = [-1, 1], cddcore = [8, 13], cddd = [20, 25], cddio = [60, 75])
             elif  ("ASICDAC_CALI_CHK" in onekey):
                 pwr_meas_fe = cfgdata[3]
                 pwr_meas_adc = cfgdata[4]
                 pwr_meas_cd = cfgdata[5]
                 bads1 = ana_fepwr2(pwr_meas_fe, vin=[1.60,1.8], cdda=[40,60], cddp=[25,35], cddo=[5,15])
-                bads2 = ana_adcpwr(pwr_meas_adc, v2p5 = [2.10, 2.35], v1p2 = [1.05, 1.15], ca2p5=[115,145], cd1p2=[0,5], cio=[3,10], cd2p5 = [15,25])
+                bads2 = ana_adcpwr(pwr_meas_adc, v2p5 = [2.10, 2.35], v1p2 = [1.05, 1.15], ca2p5=[115,145], cd1p2=[0,5], cio=[3,10], cd2p5 = [10,25])
                 bads3 = ana_cdpwr(pwr_meas_cd, vddfe = [1.6, 1.8], v1p1 = [1.15, 1.25], vddio = [2.15, 2.35], cdda = [7, 11], cddfe = [-1, 1], cddcore = [8, 13], cddd = [20, 25], cddio = [60, 75])
 
             print(onekey, 'Bads0_Pulse = {} \t Bads1_FE_Power = {}\t Bads1_ADC_Power = {}\t Bads1_CD_Power = {}'.format(bads0, bads1, bads2, bads3))
