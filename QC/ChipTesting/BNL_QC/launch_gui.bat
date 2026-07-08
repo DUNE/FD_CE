5cdddf7144f005221c7820bda7cc02e6042d398a
tcontrer <tcontrer@fnal.gov>
2026-07-08 14:43:40 -0500

    Saving launch_gui.bat

--- QC/ChipTesting/BNL_QC/launch_gui.bat
+++ QC/ChipTesting/BNL_QC/launch_gui.bat
@@ -1,5 +1,5 @@
 @echo off
 call "C:\ProgramData\miniforge3\Scripts\activate.bat" base
-cd /d "C:\Users\ppd-cap-WD-137553\FD_CE\QC\ChipTesting\BNL_QC"
+cd /d "C:\Users\ppd-cap-WD-137552\FD_CE\QC\ChipTesting\BNL_QC"
 python ..\Integration\chiptestingGUI.py
 pause
